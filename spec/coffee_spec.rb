4de97219e83cfb31311acb8ef4aeb787aa8d3c5f
rohan <kharvirohan@gmail.com>
2020-08-03 11:11:31 +0530

    Initial commit

---
+++ spec/coffee_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Coffee do
+  it "has a version number" do
+    expect(Coffee::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
